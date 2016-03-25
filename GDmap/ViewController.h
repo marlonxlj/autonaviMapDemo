cac03caa69ca6769a37b79a65e348a07fc1601c1
tangmeng11 <tangmeng@tangmengdemacbook-pro.local>
2016-03-25 18:07:35 +0800

    Initial Commit

---
+++ GDmap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDmap
+//
+//  Created by 唐萌 on 16/3/25.
+//  Copyright © 2016年 唐萌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GDmap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GDmap
+//
+//  Created by 唐萌 on 16/3/25.
+//  Copyright © 2016年 唐萌. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
